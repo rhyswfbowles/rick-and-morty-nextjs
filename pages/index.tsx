526f1ed3ca55e60ffb9b9841942016ad3386917a
Rhys Bowles <rhys.wf.bowles@gmail.com>
2021-09-24 15:56:33 +0100

    feat: run on ssl for local fetch

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,27 @@
+import https from 'https';
 import { NextPage } from 'next';
 import CharacterListContainer from '../src/containers/CharacterList';
+
+const fetchOptions = {
+    agent: new https.Agent({ rejectUnauthorized: false })
+} as RequestInit;
+
 
 const ListPage: NextPage = ({ characters }) => {
   return (
     <>
-      <CharacterListContainer title="Character Listing" charactersList={characters} />
+      <CharacterListContainer title="Character Listing" charactersList={characters}/>
     </>
   );
 };
 
-export async function getServerSideProps() {
-  const res = await fetch('https://rickandmortyapi.com/api/character');
-  const {results: characters} = await res.json();
+export async function getServerSideProps({req}) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_HOST}/api/characters?name=rick&status=alive`, fetchOptions);
+  const { results: characters } = await res.json();
 
   return {
     props: {
-      characters,
+      characters
     },
   }
 }
